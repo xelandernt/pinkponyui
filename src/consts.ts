74e89e6896518dde5caf34d7c62408a69ab93890
Alexander <alexanderlazarev0@gmail.com>
2025-12-04 22:58:59 +0100

    Minor fix.

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pink Pony Run Club';
+export const SITE_TITLE = 'Pink Pony Running Club';
 export const SITE_DESCRIPTION = 'Run Pony Run!';
 export const INSTAGRAM_URL = 'https://instagram.com/pinkponyrunningclub';
