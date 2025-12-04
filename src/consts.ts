4ad011f7b11b3cd16dae045f109b02a9d95e9d6f
alex <alexanderlazarev0@gmail.com>
2025-12-04 15:01:52 +0100

    Cleanup.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pink Pony Run Club';
+export const SITE_DESCRIPTION = 'Run Pony Run!';
